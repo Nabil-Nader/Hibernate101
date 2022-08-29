00fb20304181447720925f2ac28915c18f92f5da
nabil <nabil.nader00@gmail.com>
2022-08-30 00:07:15 +0200

    when trying to use map it is not working

--- src/main/java/com/luv2code/hibernate/demo/CreateStudentImagesListDemo.java
+++ src/main/java/com/luv2code/hibernate/demo/CreateStudentImagesListDemo.java
@@ -1,6 +1,6 @@
 package com.luv2code.hibernate.demo;
 
-import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,46 +12,42 @@ public class CreateStudentImagesListDemo {
 
 
 	public static void main(String[] args) {
-		
+
 		//create session factory
 		SessionFactory factory = new Configuration()
-									.configure("hibernate.cfg.xml")
-									.addAnnotatedClass(Student.class)
-									.buildSessionFactory();
-		
-
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+//
+//
 		//create session
-		Session session = factory.getCurrentSession();
-		
-		try {
-		//create the object
-			Student tempStudent = new Student("John","Doe","john@luv2code.com");
-			List<String> theImages = tempStudent.getImages();
-			
-			theImages.add("photo1.jpg");
-			theImages.add("photo2.jpg");
-			theImages.add("photo3.jpg");
-			theImages.add("photo4.jpg");
-			theImages.add("photo4.jpg"); //Duplicate, filtered at java level by HashSet!!!
-
-			
-		//start a transaction
-			session.beginTransaction();
-			
-		//save the object
-			System.out.println("Saving the student and images..");
-			session.persist(tempStudent);
-			
-		//commit the transaction
-			session.getTransaction().commit();
-			System.out.println("Done!!");
-		}
-		finally {
-		//clean up code
-			session.close();
-			factory.close();
-		}
-		
+//		Session session = factory.getCurrentSession();
+//
+//		try {
+//			//create the object
+//			Student tempStudent = new Student("John","Doe","john@luv2code.com");
+//			Map<String, String> theImages = tempStudent.getImages();
+//
+//			theImages.put("photo1.jpg", "Photo 1");
+//			theImages.put("photo2.jpg", "Photo 2");
+//			theImages.put("photo3.jpg", "Photo 3");
+//
+//			//start a transaction
+//			session.beginTransaction();
+//
+//			//save the object
+//			System.out.println("Saving the student and images..");
+//			session.persist(tempStudent);
+//
+//			//commit the transaction
+//			session.getTransaction().commit();
+//			System.out.println("Done!!");
+//		}
+//		finally {
+//			//clean up code
+//			session.close();
+//			factory.close();
+//		}
 
 
 	}
--- src/main/java/com/luv2code/hibernate/demo/entity/Student.java
+++ src/main/java/com/luv2code/hibernate/demo/entity/Student.java
@@ -1,7 +1,7 @@
 package com.luv2code.hibernate.demo.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
@@ -10,45 +10,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OrderColumn;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="student")
-
 public class Student {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	
+
 	@Column(name="first_name")
 	private String firstName;
-	
+
 	@Column(name="last_name")
 	private String lastName;
-	
+
 	@Column(name="email")
 	private String email;
-		
+
 	@ElementCollection
-//	@CollectionTable(name="image")
-	@CollectionTable(name="image", //defaults to student_images
-	joinColumns = @JoinColumn(name="student_id"))
-	@OrderColumn
-	@Column(name="file_name") //defaults to images
-	private List<String> images = new ArrayList<String>();
-	
-	
+	@CollectionTable(name="image")
+	@MapKeyColumn(name="file_name")
+	@Column(name="image_name")
+	private Map<String, String> images = new HashMap<String, String>();
+
+
+	public Student() {
+	}
+
 	public Student(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-	}
-
-
-	public Student() {
 	}
 
 	public int getId() {
@@ -84,11 +79,12 @@ public class Student {
 	}
 
 
-	public List<String> getImages() {
+
+	public Map<String, String> getImages() {
 		return images;
 	}
 
-	public void setImages(List<String> images) {
+	public void setImages(Map<String, String> images) {
 		this.images = images;
 	}
 
@@ -96,5 +92,5 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
+
 }
